01235f23cafe64ccc7150d2b46c73f7f93004183
Michael Fatemi <myfatemi04@gmail.com>
2020-06-02 18:54:17 -0400

    Fixed worldometers data

--- data_collection/Start_Collection.bat
+++ data_collection/Start_Collection.bat
@@ -1,1 +1,13 @@
-python server.py
+@ECHO off
+
+:top
+
+REM Run the code.
+python run_once.py
+
+REM Wait for 10 minutes.
+TIMEOUT 600
+
+REM Loop back to the top.
+
+GOTO :top
--- data_collection/data_sources/worldometers.py
+++ data_collection/data_sources/worldometers.py
@@ -1,39 +1,62 @@
 import requests
 import standards
-from data_parser import import_table
+from bs4 import BeautifulSoup
 from data_sources import source
 
-minWait = 60
-
-disallowed = [
-    "France"    # clear data disputes between the countries
-]
+def has_class(node, cls):
+    return cls in node.attrs.get("class", [])
 
 @source('live', name='Worldometers')
 def import_data():
-    results = import_table(
-        "http://www.worldometers.info/coronavirus",
-        ["#main_table_countries_today", 0],
-        {
-            "datapoint": {
-                "country": ["Country,  Other"],
-                "total": ["Total  Cases", "::number"],
-                "deaths": ["Total  Deaths", "::number"],
-                "serious": ["Serious,  Critical", "::number"],
-                "tests": ["Total  Tests", "::number"],
-                "recovered": ["Total  Recovered", "::number"]
-            }
+    data = requests.get("http://www.worldometers.info/coronavirus")
+    soup = BeautifulSoup(data.text, "html.parser")
+    table = soup.find("table", id="main_table_countries_today")
+
+    for row in table.find("tbody").findAll("tr"):
+        if row.has_attr("data-continent"):
+            continue
+        
+        columns = row.findAll("td")
+
+        rank = columns[0].text
+        name = columns[1].text
+        total = columns[2].text
+        # dtotal = columns[3].text
+        deaths = columns[4].text
+        # ddeaths = columns[5].text
+        recovered = columns[6].text
+        # drecovered = columns[7].text
+        active = columns[8].text
+        serious = columns[9].text
+        # cases_mil = columns[10].text
+        # deaths_mil = columns[11].text
+        tests = columns[12].text
+        # tests_mil = columns[13].text
+        # population = columns[14].text
+        
+        # This country has a data dispute with Worldometers
+        if name == "France":
+            continue
+        
+        row = {
+            "country": name,
+            "total": total,
+            "deaths": deaths,
+            "recovered": recovered,
+            "active": active,
+            "serious": serious,
+            "tests": tests
         }
-    )
-    
-    for result in results['datapoint']:
-        result['country'], _, _ = standards.normalize_name(result['country'], '', '')
-        if result['country'] not in disallowed:
-            yield result
+        
+        yield row
 
 def getSources():
+    """
+    I created this function to extract which sources Worldometers used for each country.
+    That way, I could find more live and state-level data.
+    """
     from bs4 import BeautifulSoup
-    import datetime
+    import datetime, time
 
     textContent = requests.get("http://www.worldometers.info/coronavirus", timeout=10).text
     soup = BeautifulSoup(textContent, "html.parser")
